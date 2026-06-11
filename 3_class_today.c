dc503ff6e926b4c086816d703cfb4bf9428d1960
Ronak <mishraronak771@gmail.com>
2026-06-11 12:26:16 +0530

    Create 3_class_today.c

---
+++ 3_class_today.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+       
+       int year = 2026;
+       int date = 11;
+       int month = 6;
+       
+    printf("today is %dth June %d\n ", date , year);
+}
