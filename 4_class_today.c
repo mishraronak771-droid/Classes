647b254ce24c51bc7770f3fe1a5429a17e497905
Ronak <mishraronak771@gmail.com>
2026-06-13 18:06:25 +0530

    Create 4_class_today.c

---
+++ 4_class_today.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void)
+{
+       
+       
+       int date = 13;
+       int months = 6;
+       int year = 2026;
+       
+       printf("%d %d %d\n", date , months , year);
+       printf("%d %d %d\n", months , date , year);
+  }
+  
+ 
